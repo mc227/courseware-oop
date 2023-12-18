38a336c2fd66291f9b3ae9da0570f812adf4d26e
mc227 <markcostales.mark@gmail.com>
2023-12-18 13:13:17 -0500

    this is before the xtra credit adding the middle name

---
+++ person.py
@@ -0,0 +1,28 @@
+"""
+docstring
+"""
+class Person:
+    """
+    docstring
+    """
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    def full_name(self):
+        """
+        docstring
+        """
+        return self.first + " " + self.last
+        
+    def formal_name(self, title):
+        """
+        docstring
+        """
+        return title + " " + self.full_name()
+
+
+
+if __name__ == "__main__":
+    person = Person("Mark", "Costales")
+    print(person.formal_name("Mr."))
