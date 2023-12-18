2df8f8188c44b9b00a2e467446d42dcd23624d6f
mc227 <markcostales.mark@gmail.com>
2023-12-18 13:21:17 -0500

    added middle name to person.py

--- person.py
+++ person.py
@@ -5,14 +5,15 @@ class Person:
     """
     docstring
     """
-    def __init__(self, first, last):
+    def __init__(self, first, middle,last):
         self.first = first
+        self.middle = middle
         self.last = last
     def full_name(self):
         """
         docstring
         """
-        return self.first + " " + self.last
+        return self.first + " " + self.middle + " " + self.last
     def formal_name(self, title):
         """
         docstring
@@ -20,5 +21,5 @@ class Person:
         return title + " " + self.full_name()
 
 if __name__ == "__main__":
-    person = Person("Mark", "Costales")
+    person = Person("Mark", "Christopher", "Costales")
     print(person.formal_name("Mr."))
