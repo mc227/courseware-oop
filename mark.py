7561070cd185ed2c5b80381517cceb7b596da88e
mc227 <markcostales.mark@gmail.com>
2023-12-18 11:48:54 -0500

    quarter

--- mark.py
+++ mark.py
@@ -2,6 +2,24 @@
 mark.py
 """
 
+def new_person(first,last):
+    """
+    docstring
+    """
+    return {"first": first, "last": last}
+
+
+def full_name(person):
+    """
+    docstring
+    """
+    return person["first"] + " " + person["last"]
+
+def formal_name(person, title):
+    """
+    docstring
+    """
+    return title + " " + full_name(person
+    )
 if __name__ == "__main__":
     print("oh hi Mark")
-    
---
+++ quarter.py
@@ -0,0 +1,13 @@
+"""
+docstring
+"""
+class Quarter:
+    """
+    docstring
+    """
+    value = 25
+
+
+if __name__ == "__main__":
+    coin = Quarter()
+    print(coin.value)
