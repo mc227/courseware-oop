472e01c17b49149e1c5a2bc3928051e9c456d4f3
mc227 <markcostales.mark@gmail.com>
2023-12-18 16:16:54 -0500

    i finished most of the extra credit classes and methods except for the last few tests

--- labs/classesandmethods_extra.py
+++ labs/classesandmethods_extra.py
@@ -42,71 +42,113 @@ CustomerAccount class to manage the nuances of this.
 >>> customer_name = 'James Jones'
 >>> account = CustomerAccount(customer_name)
 
+Did you know Python class objects have a __name__ attribute?
+>>> type(account).__name__
+'CustomerAccount'
+>>> account.name
+'James Jones'
+
+The add_invoice() method takes an instance of the Invoice class.
+
+>>> account.add_invoice(Invoice(1, customer_name, 20.0))
+>>> len(account.invoices)
+1
+>>> account.total_due()
+20.0
+>>> account.add_invoice(Invoice(2, customer_name, 25.0))
+>>> len(account.invoices)
+2
+>>> account.total_due()
+45.0
+>>> account.add_invoice(Invoice(3, customer_name, 30.0))
+>>> len(account.invoices)
+3
+>>> account.total_due()
+75.0
+
+>>> unpaid = account.unpaid_invoices()
+>>> len(unpaid)
+3
+>>> type(unpaid[0]).__name__
+'Invoice'
+>>> unpaid[0].number
+1
+>>> unpaid[0].amount
+20.0
+
+>>> account.apply_payment(20)
+>>> now_unpaid = account.unpaid_invoices()
+>>> len(now_unpaid)
+2
+>>> now_unpaid[0].number
+2
+>>> account.total_due()
+55.0
+
+>>> account.apply_payment(10)
+>>> account.total_due()
+45.0
+>>> len(account.unpaid_invoices())
+2
+
+>>> account.apply_payment(45)
+>>> len(account.unpaid_invoices())
+0
 
 '''
 
-# Did you know Python class objects have a __name__ attribute?
-# >>> type(account).__name__
-# 'CustomerAccount'
-# >>> account.name
-# 'James Jones'
-
-# The add_invoice() method takes an instance of the Invoice class.
-
-# >>> account.add_invoice(Invoice(1, customer_name, 20.0))
-# >>> len(account.invoices)
-# 1
-# >>> account.total_due()
-# 20.0
-# >>> account.add_invoice(Invoice(2, customer_name, 25.0))
-# >>> len(account.invoices)
-# 2
-# >>> account.total_due()
-# 45.0
-# >>> account.add_invoice(Invoice(3, customer_name, 30.0))
-# >>> len(account.invoices)
-# 3
-# >>> account.total_due()
-# 75.0
-
-# >>> unpaid = account.unpaid_invoices()
-# >>> len(unpaid)
-# 3
-# >>> type(unpaid[0]).__name__
-# 'Invoice'
-# >>> unpaid[0].number
-# 1
-# >>> unpaid[0].amount
-# 20.0
-
-# >>> account.apply_payment(20)
-# >>> now_unpaid = account.unpaid_invoices()
-# >>> len(now_unpaid)
-# 2
-# >>> now_unpaid[0].number
-# 2
-# >>> account.total_due()
-# 55.0
-
-# >>> account.apply_payment(10)
-# >>> account.total_due()
-# 45.0
-# >>> len(account.unpaid_invoices())
-# 2
-
-# >>> account.apply_payment(45)
-# >>> len(account.unpaid_invoices())
-# 0
 # Write your code here:
+
 class CustomerAccount:
-    """
-    docstring
-    """
     def __init__(self, customer_name):
+        self.name = customer_name
+        self.invoices = []
+
+    def add_invoice(self, invoice):
+        self.invoices.append(invoice)
+
+    def total_due(self):
+        due = 0
+        for invoice in self.invoices:
+            due += invoice.amount_due()
+        return due
+        # Alternatively: use a generator comprehension.
+        # (Learn these in the "Scaling Python with Generators" course.)
+        return sum( invoice.amount_due()
+                    for invoice in self.invoices )
+
+    def unpaid_invoices(self):
+        items = []
+        for invoice in self.invoices:
+            if not invoice.is_fully_paid():
+                items.append(invoice)
+        return items
+        # Again, you can do this more simply with a comprehension:
+        return [ invoice for invoice in self.invoices
+                 if not invoice.is_fully_paid() ]
+
+    def apply_payment(self, amount):
+        unpaid = self.unpaid_invoices()
+        while amount > 0:
+            invoice = unpaid.pop(0)
+            partial = min(amount, invoice.amount_due())
+            invoice.add_payment(partial)
+            amount -= partial
+
+    # Another way to do this:
+    # (Can you tell the difference between the two versions, algorithmically?)
+    def apply_payment(self, amount):
         """
         docstring
         """
-        self.customer_name = customer_name
+        for invoice in self.unpaid_invoices():
+            due = invoice.amount_due()
+            if due < amount:
+                invoice.add_payment(due)
+                amount -= due
+            else:
+                invoice.add_payment(amount)
+                break
 
 class Invoice:
     """
