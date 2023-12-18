69b17bab2ac6eaea7fd4745dac353554ea203a80
mc227 <markcostales.mark@gmail.com>
2023-12-18 13:14:17 -0500

    fixed spacing

--- person.py
+++ person.py
@@ -8,20 +8,16 @@ class Person:
     def __init__(self, first, last):
         self.first = first
         self.last = last
-
     def full_name(self):
         """
         docstring
         """
         return self.first + " " + self.last
-        
     def formal_name(self, title):
         """
         docstring
         """
         return title + " " + self.full_name()
-
-
 
 if __name__ == "__main__":
     person = Person("Mark", "Costales")
